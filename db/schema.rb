f7736d62ac55f4c53f41334bf636afcc1107fac9
andybsb85 <andybsb85@hotmail.com>
2014-07-20 14:23:30 +0000

    add region

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140720012704) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "countries", force: true do |t|
     t.string   "name"
     t.text     "description"
@@ -31,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140720012704) do
 
   create_table "regions", force: true do |t|
     t.string   "name"
+    t.integer  "country_id"
     t.string   "image_url"
     t.text     "description"
     t.datetime "created_at"
@@ -53,7 +57,7 @@ ActiveRecord::Schema.define(version: 20140720012704) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end

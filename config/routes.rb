a0367954c1cce03f783f18f5aac5c9b6ed00d229
andybsb85 <andybsb85@hotmail.com>
2014-07-24 22:15:59 +0000

    background image

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :countries
 
  # devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
-  devise_for :users, controllers: {registrations: 'registrations'}, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, controllers: {registrations: 'registrations', session: 'sessions'}, path_names: {sign_in: "login", sign_out: "logout"}
  
   get 'welcome/index'
   
